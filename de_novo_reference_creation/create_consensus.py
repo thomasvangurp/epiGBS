1d0a31676e77af574bf4313fad3e0c04a64978a4
Thomas van Gurp <thomasvangurp@gmail.com>
2016-03-04 16:54:49 +0100

    Speedup of making consensus. Names with watson and crick coverage as obtained from bam removed as this takes too much time.

--- de_novo_reference_creation/create_consensus.py
+++ de_novo_reference_creation/create_consensus.py
@@ -38,45 +38,46 @@ def watson_crick_count(args, region):
 
 def make_ref2(args):
     """"Make a concensus from watson and crick"""
-    watson = SeqIO.parse(open(args.watson,'r'),'fasta')
-    crick = SeqIO.parse(open(args.crick,'r'),'fasta')
+    watson = open(args.watson,'r')
+    crick = open(args.crick,'r')
     out = open(args.consensus, 'w')
+    count = 0
     try:
         while True:
-            try:
-                w = watson.next()
-                c = crick.next()
-            except StopIteration:
+            w = [watson.readline()[1:-1],watson.readline()[:-1].upper()]
+            c = [crick.readline()[1:-1],crick.readline()[:-1].upper()]
+            if w == []:
                 break
             while True:
-                if w.name == c.name:
+                if w[0] == c[0]:
                     break
                 else:
-                    try:
-                        if int(w.name) > int(c.name):
-                            c = crick.next()
-                        else:
-                            w = watson.next()
-                    except StopIteration:
-                        return 0
+                    if int(w[0]) > int(c[0]):
+                        c = [crick.readline()[1:-1],crick.readline()[:-1]]
+                    else:
+                        w = [watson.readline()[1:-1],watson.readline()[:-1]]
+                if w == []:
+                    break
             out_seq = ''
-            for wb, cb in izip(w.seq, c.seq):
-                if wb.lower() == cb.lower():
+            count +=1
+            if not count%1000:
+                print count
+            for wb, cb in izip(w[1], c[1]):
+                if wb == cb:
                     out_seq += wb
-                elif wb in 'tT' and cb in 'cC':
+                elif wb == 'T' and cb == 'C':
                     out_seq += 'C'
-                elif wb in 'gG' and cb in 'aA':
+                elif wb  == 'G' and cb == 'A':
                     out_seq += 'G'
-                elif wb in 'nN':
+                elif wb  == 'N':
                     out_seq += cb
-                elif cb in 'nN':
+                elif cb == 'N':
                     out_seq += wb
                 else:
-                    print wb, cb
-            str_out = watson_crick_count(args, w.name)
-            record_out = '>%s\n%s\n'%(w.name+str_out, out_seq.upper())
-            if out_seq:
-                out.write(record_out)
+                    out_seq += 'N'
+            # str_out = watson_crick_count(args, w[0])
+            record_out = '>%s\n%s\n'%(w[0],out_seq)
+            out.write(record_out)
     except ValueError:
         return None
 
