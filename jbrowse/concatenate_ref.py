221f7a867d8c637b49ffd8175d28879b8084b193
Thomas van Gurp <thomasvangurp@gmail.com>
2016-10-07 20:28:34 -0400

    First serious write-up of concatenate_ref

--- jbrowse/concatenate_ref.py
+++ jbrowse/concatenate_ref.py
@@ -1,5 +1,4 @@
 """concatenate reference sequence"""
-
 import argparse
 import os
 import pysam
@@ -8,16 +7,16 @@ import pysam
 def parse_args():
     """Pass command line arguments"""
 
-    parser = argparse.ArgumentParser(description='use bwameth for mapping reads')
+    parser = argparse.ArgumentParser(description='Concatenate reference for display in jbrowse')
     parser.add_argument('-o', '--output_dir', help='output directory')
-    parser.add_argument('-i', '--input_dir', help='input directory')
+    parser.add_argument('-i', '--input_dir', help='input directory with bam files')
     parser.add_argument('-r', '--ref', help='reference sequence in')
     args = parser.parse_args()
     if 'input_dir' in args:
-        if os.path.exists(os.path.join(args.input_dir, 'watson.bam')) and args.watson is None:
-            args.watson = os.path.join(args.input_dir, 'watson.bam')
-        if os.path.exists(os.path.join(args.input_dir, 'crick.bam')) and args.crick is None:
-            args.crick = os.path.join(args.input_dir, 'crick.bam')
+        if os.path.exists(os.path.join(args.input_dir, 'watson.dedup.bam')) and args.watson is None:
+            args.watson = os.path.join(args.input_dir, 'watson.dedup.bam')
+        if os.path.exists(os.path.join(args.input_dir, 'crick.dedup.bam')) and args.crick is None:
+            args.crick = os.path.join(args.input_dir, 'crick.dedup.bam')
     if not os.path.exists(args.output_dir):
         os.mkdir(args.output_dir)
     return args
@@ -25,6 +24,22 @@ def parse_args():
 def coverage_ref(args):
     """Get coverage of reference sequence"""
 
+def parse_bam(bam):
+    """parse bam file to get mapping per contig and individual"""
+    mapping_dict = {}
+    file_handle = pysam.AlignmentFile(bam)
+    for read in file_handle:
+        print ''
+
+
+
+
+def coverage_ref(args):
+    """Get coverage of reference sequence"""
+    #parse watson
+    mapping_watson = parse_bam(args.watson)
+    #parse crick
+    mapping_crick = parse_bam(args.crick)
 
 def rewrite_bam(args):
     """split bam file using pysam"""
