c3a33246b1756981e5cca4dc598c785ccfb76184
Thomas van Gurp <thomasvangurp@gmail.com>
2016-12-21 12:17:06 -0400

    make sure output bam files with duplicates marked are sorted and indexed

--- mapping_varcall/filtering/mark_PCR_duplicates.py
+++ mapping_varcall/filtering/mark_PCR_duplicates.py
@@ -2,6 +2,8 @@
 import pysam
 import os
 import sys
+import tempfile
+import subprocess
 import argparse
 from Bio import SeqIO
 from Bio import Restriction
@@ -70,8 +72,9 @@ def remove_PCR_duplicates(bam_in, bam_out, ref):
     """Remove PCR duplicates and non-paired PE-reads per cluster"""
     #TODO: implement as module to be ran concurrently with appropriate statistics.
     clusters = SeqIO.parse(open(ref),'fasta')
+    tmp_bam = tempfile.NamedTemporaryFile(suffix="temp_unsorted.bam", dir='/tmp/',delete=False)
     handle = pysam.AlignmentFile(bam_in,'rb')
-    out_handle = pysam.AlignmentFile(bam_out,'wb', template=handle)
+    out_handle = pysam.AlignmentFile(tmp_bam,'wb', template=handle)
     read_count = {}
     for cluster in clusters:
         #TODO: detect enzyme from barcode file to enable other combinations.
@@ -187,7 +190,20 @@ def remove_PCR_duplicates(bam_in, bam_out, ref):
         else:
             print '%s \t%s \t0\t0%%' % (key, count)
     out_handle.close()
-    pysam.index(bam_out)
+    cmd = ['samtools sort %s > %s' % (tmp_bam.name, bam_out )]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, executable='/bin/bash')
+    stdout, stderr = p.communicate()
+    stdout = stdout.replace('\r', '\n')
+    stderr = stderr.replace('\r', '\n')
+    print stdout
+    print stderr
+    cmd = ['samtools index %s;rm %s' % (bam_out, tmp_bam.name)]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, executable='/bin/bash')
+    stdout, stderr = p.communicate()
+    stdout = stdout.replace('\r', '\n')
+    stderr = stderr.replace('\r', '\n')
+    print stdout
+    print stderr
     return 0
 
 def main():
